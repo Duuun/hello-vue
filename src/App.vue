5788c7d6cc6588a1cbab749e2a144d1bcb270e42
Daisuke Murakami <daisuke@mbp.local>
2018-07-02 03:17:16 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="header">
+      <router-link to="/">
+        top
+      </router-link>
+    </div>
+    <div class="about">
+      <router-link to="about">
+        about
+      </router-link>
+    </div>
+    <div class="contact">
+      <router-link to="contact">
+        contact
+      </router-link>
+    </div>
+    <div class="content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/pages/about.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        ここはAboutページです。
+    </div>
+</template>
